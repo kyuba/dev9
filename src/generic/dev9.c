fe3f414ffc61a5c261b4fc4d0a9f2f27ef7b237b
Magnus Deininger <dma05@web.de>
2008-10-03 17:04:53 +0100

    updated skeleton build script, taken from duat

---
+++ src/generic/dev9.c
@@ -0,0 +1,76 @@
+/*
+ *  dev9.c
+ *  libcurie
+ *
+ *  Created by Magnus Deininger on 03/10/2008.
+ *  Copyright 2008 Magnus Deininger. All rights reserved.
+ *
+ */
+
+/*
+ * Copyright (c) 2008, Magnus Deininger All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer. *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution. *
+ * Neither the name of the project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <curie/multiplex.h>
+#include <curie/memory.h>
+
+#include <duat/9p-server.h>
+#include <duat/filesystem.h>
+#include <duat/sxfs.h>
+
+static void *rm_recover(unsigned long int s, void *c, unsigned long int l)
+{
+    exit(22);
+    return (void *)0;
+}
+
+static void *gm_recover(unsigned long int s)
+{
+    exit(23);
+    return (void *)0;
+}
+
+int main(void) {
+    struct dfs *fs;
+
+    set_resize_mem_recovery_function(rm_recover);
+    set_get_mem_recovery_function(gm_recover);
+
+    dfs_update_user  ("nyu",     1000);
+    dfs_update_group ("kittens", 100);
+
+    multiplex_d9s();
+
+    fs = dfs_create ();
+
+    multiplex_add_d9s_socket ("/tmp/dev9-duat-socket", fs);
+
+    while (multiplex() != mx_nothing_to_do);
+
+    return 0;
+}
